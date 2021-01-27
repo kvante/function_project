a98b566afe4993784b9e60fa4be72c23471621b1
Aleksandr Shishko <ghostas@tut.by>
2021-01-27 20:12:37 +0300

    created lessen5

---
+++ les_5.py
@@ -0,0 +1,16 @@
+
+# def my_func(a,b):
+#     summ = a +b
+#     print(f'{a} + {b} = {summ}')
+#     return  summ
+#
+# my_func(4 ,5)
+
+def print_name(a):
+    name = a
+    print(f'Hello, {name}')
+
+a = ['alex', 'dima', 'maks','oleg', 'misha']
+
+for i in a:
+    print_name(i)
